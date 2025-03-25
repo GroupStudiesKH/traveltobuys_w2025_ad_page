8aaf2ab45e016c74e24b6b6d9bcb978b63a600cd
mafiapatch <g8071107@gmail.com>
2025-03-25 11:16:41 +0800

    fix(样式和交互): 优化移动端动画效果和滚动体验
    在移动端设备上，调整了动画的触发阈值和速度，以提升用户体验。同时，添加了 `-webkit-overflow-scrolling: touch` 以改善滚动流畅度。此外，更新了屏幕断点以适应更多设备。

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,76 +3,116 @@ import { useRouter, useRoute } from "vue-router";
 import { onMounted, ref } from "vue";
 
 export default {
-  components: {
-  },
+  components: {},
   setup() {
     const router = useRouter();
     const screenRefs = ref([]);
 
     onMounted(() => {
       const activeIndices = ref(new Set());
+      const isMobile = window.innerWidth < 768;
 
-      const defaultObserver = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            const index = Array.from(screenRefs.value).indexOf(entry.target);
-            if (entry.isIntersecting && !activeIndices.value.has(index)) {
-              entry.target.classList.add("animate");
-              activeIndices.value.add(index);
-            }
-          });
-        },
-        { threshold: 0.5 }
-      );
+      if (isMobile) {
 
-      const screen25_4Observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            const index = Array.from(screenRefs.value).indexOf(entry.target);
-            if (entry.isIntersecting && !activeIndices.value.has(index)) {
-              entry.target.classList.add("animate");
-              activeIndices.value.add(index);
-            }
-          });
-        },
-        { threshold: 0.1 }
-      );
-
-      screenRefs.value = Array.from(
-        document.querySelectorAll(".screen_container")
-      );
-
-      screenRefs.value.forEach((el) => {
-
-        switch (el.parentElement.id) {
-          case "screen_25_3":
-          case "screen_25_2":
-          case "screen_25_4":
-          case "screen_25_5":
-          case "screen_25_51":
-          case "screen_25_52":
-          case "screen_25_6":
-          case "screen_25_7":
-          case "screen_25_8":
-          case "screen_25_10":
-          case "screen_25_12":
-            defaultObserver.observe(el)
-            break;
-          case "screen_25_9":
-          case "screen_25_11":
-            screen25_4Observer.observe(el);
-            break;
-          default:
-            break;
-        }
+        const defaultObserver = new IntersectionObserver(
+          (entries) => {
+            entries.forEach((entry) => {
+              const index = Array.from(screenRefs.value).indexOf(entry.target);
+              if (entry.isIntersecting && !activeIndices.value.has(index)) {
+                entry.target.classList.add("animate");
+                activeIndices.value.add(index);
+                console.log(activeIndices.value);
+              }
+            });
+          },
+          { threshold: 0.1 }
+        );
 
 
-      });
+        screenRefs.value = Array.from(
+          document.querySelectorAll(".screen_container")
+        );
 
+        screenRefs.value.forEach((el) => {
+          switch (el.parentElement.id) {
+            case "screen_25_3":
+            case "screen_25_2":
+            case "screen_25_4":
+            case "screen_25_5":
+            case "screen_25_51":
+            case "screen_25_52":
+            case "screen_25_6":
+            case "screen_25_7":
+            case "screen_25_8":
+            case "screen_25_10":
+            case "screen_25_12":
+            case "screen_25_9":
+            case "screen_25_11":
+              defaultObserver.observe(el);
+              break;
+            default:
+              break;
+          }
+        });
+
+      } else {
+        const defaultObserver = new IntersectionObserver(
+          (entries) => {
+            entries.forEach((entry) => {
+              const index = Array.from(screenRefs.value).indexOf(entry.target);
+              if (entry.isIntersecting && !activeIndices.value.has(index)) {
+                entry.target.classList.add("animate");
+                activeIndices.value.add(index);
+              }
+            });
+          },
+          { threshold: 0.5 }
+        );
+
+        const screen25_4Observer = new IntersectionObserver(
+          (entries) => {
+            entries.forEach((entry) => {
+              const index = Array.from(screenRefs.value).indexOf(entry.target);
+              if (entry.isIntersecting && !activeIndices.value.has(index)) {
+                entry.target.classList.add("animate");
+                activeIndices.value.add(index);
+              }
+            });
+          },
+          { threshold: 0.1 }
+        );
+
+        screenRefs.value = Array.from(
+          document.querySelectorAll(".screen_container")
+        );
+
+        screenRefs.value.forEach((el) => {
+          switch (el.parentElement.id) {
+            case "screen_25_3":
+            case "screen_25_2":
+            case "screen_25_4":
+            case "screen_25_5":
+            case "screen_25_51":
+            case "screen_25_52":
+            case "screen_25_6":
+            case "screen_25_7":
+            case "screen_25_8":
+            case "screen_25_10":
+            case "screen_25_12":
+              defaultObserver.observe(el);
+              break;
+            case "screen_25_9":
+            case "screen_25_11":
+              screen25_4Observer.observe(el);
+              break;
+            default:
+              break;
+          }
+        });
+      }
     });
 
-    return {
-    };
+    return {};
   },
 };
 </script>
@@ -81,56 +121,61 @@ export default {
   <main>
     <div id="screen_25_1">
       <div class="screen_container animate">
-
         <div class="left_side">
-          <img src="/assets/img/W25-01-banner2.webp" style="width: 80%; margin: 5% 0">
-          <img src="/assets/img/W25-01-food.webp">
+          <img
+            src="/assets/img/W25-01-banner2.webp"
+            style="width: 80%; margin: 5% 0"
+          />
+          <img src="/assets/img/W25-01-food.webp" />
         </div>
 
-        <img src="/assets/img/W25-01-banner.webp" class="banner">
-        <img src="/assets/img/W25-01-banner1.webp" class="banner1">
+        <img src="/assets/img/W25-01-banner.webp" class="banner" />
+        <img src="/assets/img/W25-01-banner1.webp" class="banner1" />
 
         <div class="right_side">
-          <img src="/assets/img/W25-01-food2.webp">
-          <img src="/assets/img/W25-01-banner3.webp" style="width: 60%; margin: 5% 0">
-          <img src="/assets/img/W25-01-food3.webp">
+          <img src="/assets/img/W25-01-food2.webp" />
+          <img
+            src="/assets/img/W25-01-banner3.webp"
+            style="width: 60%; margin: 5% 0"
+          />
+          <img src="/assets/img/W25-01-food3.webp" />
         </div>
       </div>
     </div>
 
     <div id="screen_25_2">
       <div class="screen_container">
-        <img src="/assets/img/W25-01-screen2.webp" class="banner">
+        <img src="/assets/img/W25-01-screen2.webp" class="banner" />
       </div>
     </div>
 
     <div id="screen_25_3">
       <div class="screen_container">
-        <img src="/assets/img/W25-01-screen3.webp" class="banner">
+        <img src="/assets/img/W25-01-screen3.webp" class="banner" />
       </div>
     </div>
 
     <div id="screen_25_4">
       <div class="screen_container">
-        <img src="/assets/img/W25-01-screen4.webp" class="banner">
+        <img src="/assets/img/W25-01-screen4.webp" class="banner" />
       </div>
     </div>
 
     <div id="screen_25_5">
       <div class="screen_container">
-        <img src="/assets/img/W25-01-screen5.webp" class="banner">
+        <img src="/assets/img/W25-01-screen5.webp" class="banner" />
       </div>
     </div>
 
     <div id="screen_25_51">
       <div class="screen_container">
-        <img src="/assets/img/W25-01-screen6.webp" class="banner">
+        <img src="/assets/img/W25-01-screen6.webp" class="banner" />
       </div>
     </div>
 
     <div id="screen_25_52">
       <div class="screen_container">
-        <img src="/assets/img/W25-01-screen7.webp" class="banner">
+        <img src="/assets/img/W25-01-screen7.webp" class="banner" />
       </div>
     </div>
 
@@ -160,24 +205,26 @@ export default {
 
     <div id="screen_25_10">
       <div class="screen_container animate">
-        <img src="/assets/img/W25_03_screen1.webp" class="banner">
+        <img src="/assets/img/W25_03_screen1.webp" class="banner" />
       </div>
     </div>
 
     <div id="screen_25_11">
       <div class="screen_container">
-        <img src="/assets/img/W25_03_screen2.webp" class="banner">
-        <img src="/assets/img/W25_03_screen3.webp" class="banner">
+        <img src="/assets/img/W25_03_screen2.webp" class="banner" />
+        <img src="/assets/img/W25_03_screen3.webp" class="banner" />
       </div>
     </div>
 
     <div id="screen_25_12">
-      <div class="screen_container">
-      </div>
+      <div class="screen_container"></div>
     </div>
 
-    <a class="add" href="https://manage.tigerairtw.com/zh-TW/search-order?_gl=1*1e2lpps*_gcl_aw*R0NMLjE3NDIyODkyNjcuQ2owS0NRandzLVMtQmhEMkFSSXNBTHNzRzBaYXVkUDR1aVBQbDZLNG1TQm1hWklrYjhsbGwwejRRV2U2SElHeUdXU1VETWZCaU1GU0Jvc2FBbWRTRUFMd193Y0I.*_gcl_au*MTQyMjQxMDc5Mi4xNzQwNjM1Nzk4*_ga*MjA3NzQ2MjA4NS4xNzMyODQ2NDQ2*_ga_SNXJDVLRNM*MTc0Mjc4MDI2Ni40NC4wLjE3NDI3ODAyNjYuNjAuMC4w" target="_blank"><img src="/assets/img/W25-01-add.webp"></a>
-
-
+    <a
+      class="add"
+      href="https://manage.tigerairtw.com/zh-TW/search-order?_gl=1*1e2lpps*_gcl_aw*R0NMLjE3NDIyODkyNjcuQ2owS0NRandzLVMtQmhEMkFSSXNBTHNzRzBaYXVkUDR1aVBQbDZLNG1TQm1hWklrYjhsbGwwejRRV2U2SElHeUdXU1VETWZCaU1GU0Jvc2FBbWRTRUFMd193Y0I.*_gcl_au*MTQyMjQxMDc5Mi4xNzQwNjM1Nzk4*_ga*MjA3NzQ2MjA4NS4xNzMyODQ2NDQ2*_ga_SNXJDVLRNM*MTc0Mjc4MDI2Ni40NC4wLjE3NDI3ODAyNjYuNjAuMC4w"
+      target="_blank"
+      ><img src="/assets/img/W25-01-add.webp"
+    /></a>
   </main>
 </template>
